83d544989f32975d7cf28d757cefe22e9d81f50b
Mia Simone <cph-md291@cphbusiness.dk>
2020-10-22 13:14:37 +0200

    Personfacade html done, missing js functions

--- src/index.js
+++ src/index.js
@@ -9,41 +9,58 @@ document.getElementById("all-content").style.display = "block"
   Add your JavaScript for all exercises Below or in separate js-files, which you must the import above
 */
 
-/* JS For Exercise-1 below */
+/* JS For Search */
+/*
+function makePersonsTable(){
+  personFacade.getAllPersons()
+  .then(data => {
+    const persons = data.all;
+    const tableRows = persons.map(person => `
+    <tr>
+      <td>${person.id}</td>
+      <td>${person.email}</td>
+      <td>${person.firstName}</td>
+      <td>${person.lastName}</td>
+      <td>${person.phones}</td>
+      <td>${person.hobbies}</td>
+    </tr>  
+    `)
+    const tableRowsAssString = tableRows.join("");
+    document.getElementById("tbody").innerHTML = tableRowsAssString;
+    document.getElementById("error").innerHTML = "";
+  })
+  .catch(err =>{
+    if(err.status){
+      err.fullError.then(e=> document.getElementById("error").innerHTML = 
+      `<p class="alert alert-danger" role="alert">${e.message}</p>`)
+    }
+    else{console.log("Network error"); }
+  });
+}
+makePersonsTable();
 
 
-/* JS For Exercise-2 below */
-
-
-
-/* JS For Exercise-3 below */
-
-
-/* 
+/*
 Do NOT focus on the code below, UNLESS you want to use this code for something different than
 the Period2-week2-day3 Exercises
 */
 
 function hideAllShowOne(idToShow) {
-  document.getElementById("about_html").style = "display:none"
-  document.getElementById("ex1_html").style = "display:none"
-  document.getElementById("ex2_html").style = "display:none"
-  document.getElementById("ex3_html").style = "display:none"
+  document.getElementById("home_html").style = "display:none"
+  document.getElementById("search_html").style = "display:none"
   document.getElementById(idToShow).style = "display:block"
 }
 
 function menuItemClicked(evt) {
   const id = evt.target.id;
   switch (id) {
-    case "ex1": hideAllShowOne("ex1_html"); break
-    case "ex2": hideAllShowOne("ex2_html"); break
-    case "ex3": hideAllShowOne("ex3_html"); break
-    default: hideAllShowOne("about_html"); break
+    case "search": hideAllShowOne("search_html"); break
+    default: hideAllShowOne("home_html"); break
   }
   evt.preventDefault();
 }
 document.getElementById("menu").onclick = menuItemClicked;
-hideAllShowOne("about_html");
+hideAllShowOne("home_html");
 
 
 
---
+++ src/personFacade.js
@@ -0,0 +1,60 @@
+
+const URL = "https://ditlevsoftware.com/ca2/api/person";
+
+function makeOptions(method, body) {
+    var opts =  {
+      method: method,
+      headers: {
+        "Content-type": "application/json",
+        "Accept": "application/json"
+      }
+    }
+    if(body){
+      opts.body = JSON.stringify(body);
+    }
+    return opts;
+}
+  
+function handleHttpErrors(res){
+    console.log(res)
+    if(!res.ok){
+    return Promise.reject({status: res.status, fullError: res.json() })
+    }
+    return res.json();
+}
+
+function getAllPersons(){
+    return fetch(URL + "all")
+        .then(handleHttpErrors)
+}
+
+function addPerson(person){
+    const options = makeOptions("POST", person)
+    return fetch(URL, options)
+        .then(handleHttpErrors)
+}
+
+function deletePerson(id){
+    const options = makeOptions("DELETE");
+    return fetch(URL + id, options)
+    .then(handleHttpErrors)
+}
+
+function editPerson(person){
+    const options = makeOptions("PUT", person);
+    return fetch(URL + person.id, options)
+    .then(handleHttpErrors)
+}
+
+const personFacade = {
+    getAllPersons,
+    addPerson,
+    deletePerson,
+    editPerson,
+}
+
+   
+export default personFacade;
+
+
+
